cc11e3b9d5c93071f94419d930e753f1e3f6d30f
Danwand <45515141+DanBrown47@users.noreply.github.com>
2019-11-13 20:31:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import hashlib
+#!/usr/bin/env python
+#
 
+#
+# Converts text file with plain text file to hashed format
+#
+#
+# (c) 2019  Danwand N S <Titanium Hitler>
+# co-created by Abhinand N S <> github.com/abhi-nvr-quits
+import hashlib
+import os
+
+from optparse import OptionParser
 
 
 def main():
@@ -7,14 +18,31 @@ def main():
 		fo = open("b.txt","w")
 		c=0
 		for i in afile:
-			#The strings are hashed though library hasllib
-			result = hashlib.md5(str.encode(i)) 
-			
+			#print(i,j)
+			result = hashlib.md5(str.encode(i))
+			#print(result.hexdigest())
 			fo.write(result.hexdigest())
 			fo.write("\n")
 			c = c+ 1
 			print("count : " + str(c))
-			
+
 		fo.close()
-main()
+
+if __name__ == '__main__':
+    parser = OptionParser(usage="Usage: python %prog [FILE]... ")
+
+    options, args = parser.parse_args()
+
+    if not args:
+        parser.print_usage()
+		print("File not in txt")
+        exit(1)
+
+
+    for arg in args:
+        if os.path.isfile(arg):
+            main()
+
+
+
 
